433656ef5f35bfdef363251bd0a5176169198b4d
Tim Golla <tim.golla.official@gmail.com>
2020-03-17 21:05:36 +0100

    - fix: ignore extensions for string replacement in tex - fix: process longest filenames first - fixes problem of filenames being prefixes of longer filenames

--- latex_cleaner.py
+++ latex_cleaner.py
@@ -46,7 +46,9 @@ def latex_clean(args):
                 other_files_out.append(current_filename_out)
     tex_contents = []
     all_files_in = tex_files_in + other_files_in
+    all_files_in = sorted(all_files_in,key=len)[::-1]
     all_files_out = tex_files_out + other_files_out
+    all_files_out = sorted(all_files_out,key=len)[::-1]
     for i in range(len(tex_files_in)):
         texfilename = os.path.join(args.inputdir, tex_files_in[i])
         f = open(texfilename, "r", errors=args.errors)
@@ -66,12 +68,14 @@ def latex_clean(args):
             tex_contents.append(f.read())
         if args.remove_subdirs:
             for j in range(len(all_files_in)):
-                tex_contents[i] = tex_contents[i].replace(all_files_in[j],all_files_out[j])
+                fin, ext = os.path.splitext(all_files_in[j])
+                fout, ext = os.path.splitext(all_files_out[j])
+                tex_contents[i] = tex_contents[i].replace(fin,fout)
         f.close()
     used = np.zeros(len(other_files_in), dtype=bool)
     notfound = np.ones(len(other_files_in), dtype=bool)
-    for i in range(len(other_files_in)):
-        myf, ext = os.path.splitext(other_files_in[i])
+    for i in range(len(other_files_out)):
+        myf, ext = os.path.splitext(other_files_out[i])
         kp = False
         for pref in args.keep_prefixes:
             if myf.startswith(pref):
